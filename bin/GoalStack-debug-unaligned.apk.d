b9aae65492601ac6c8e7f1038ba769bc9bd75bd7
pf8363 <pf8363@bu.edu>
2013-11-19 19:38:23 -0500

    Hello

--- bin/GoalStack-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/GoalStack-debug-unaligned.apk : \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/GoalStack.ap_ \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes.dex \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/libs/cordova-3.1.0.jar \
--- bin/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes.dex : \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/BuildConfig.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/GoalStack.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/R$attr.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/R$drawable.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/R$string.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/R$xml.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/classes/edu/bu/goalstack/R.class \
-/Users/dblair/Development/workspace/goalstack-1/platforms/android/bin/dexedLibs/cordova-3.1.0-05aaf24d215c5bf440206b1bd4e90b3d.jar \
